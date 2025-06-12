318f90dc6b0c9d34b29713bc6d32d93e7e0a0c64
POO <1202mariag@gmail.com>
2025-06-12 09:21:38 -0500

    Creacion de paquetes  y organizado por clases

--- src/Modelo/Docente.java
+++ src/Modelo/Docente.java
@@ -3,4 +3,21 @@ package Modelo;
 public class Docente {
     private String nombre;
     private String especialidad;
+
+    public Docente(String nombre, String especialidad) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getEspecialidad() { return especialidad; }
+
+    public void  mostrarPerfil(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Especialidad: " + especialidad);
+
+    }
+
 }
+
+
